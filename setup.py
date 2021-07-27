1923bc2b343a8ecc47ce400a8c21abd5f221471b
Jorge Riveros <christian.riveros@outlook.com>
2021-07-27 18:32:56 -0500

    updating setup

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ with open("README.md", "r") as fh:
 INSTALL_REQUIRES = ['requests']
 
 setuptools.setup(
-    name="thread_decorator",
-    version="0.2",
+    name="netscaler_module",
+    version="0.1",
     author="Jorge Riveros",
     author_email="christian.riveros@outlook.com",
     license='MIT',
-    description='A Python package for Multithread processing',
+    description='A Python package to query Netscaler Information',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cocuni80/thread_decorator",
-    packages=['thread_decorator'],
+    url="https://github.com/cocuni80/netscaler_module",
+    packages=['module'],
     install_requires=INSTALL_REQUIRES,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.x',
 )
