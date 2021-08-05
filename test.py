634c35db3076a74458db29f1cc35dbc9a8db45e0
Jorge Riveros <christian.riveros@outlook.com>
2021-08-04 23:55:52 -0500

    updating Readme

--- test.py
+++ test.py
@@ -1,28 +1,9 @@
-# Netscaler Module
-Netscaler module for python 3.x
-
-## Requirements
-Python 3+
-
-## Installation
-```shell
-$ pip install netscaler_module
-```
-
-## Quickstart
-Import the `netscaler_module` library to use the functions
-```python
 from netscaler_module import NitroClass
-
-....
-```
-Create a `DATABASE` variable and append the dicts response from nitro class.
-Below an example.
-```python
 
 def get_ns(**kwargs):
     global DATABASE
     ns = NitroClass(**kwargs)
+    ns._conexion = 'HTTP'
     ns.login()
     if ns.master:
         data = ns.get_lbvservers_binding_partitions()
@@ -36,16 +17,15 @@ DATABASE = list()
 
 if __name__ == '__main__':
     ns_pool = [
-        '192.168.1.1',
-        '192.168.1.2',
+        '192.168.2.100',
+        '192.168.2.101',
     ]
     password = {
         'username': 'nsroot',
-        'password': 'XXXXXXX'
+        'password': 'nsroot'
     }
     for ns_ip in ns_pool:
         temp = {'ip': ns_ip} | password
         get_ns(**temp)
     
     print(DATABASE)
-```
